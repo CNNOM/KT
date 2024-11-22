285c2e46c7cbe59b4d8caf38af42e2fdf89015fb
Алексей Зенков <zenkov.m700@mail.ru>
2024-11-22 21:55:01 +0300

    [KT-13] Вывод всех пользователь перенесён на домашнюю страницу

--- app/src/main/java/com/example/group_project_vstu/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/group_project_vstu/ui/home/HomeViewModel.java
@@ -1,19 +1,48 @@
 package com.example.group_project_vstu.ui.home;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+import com.example.group_project_vstu.AppDatabase;
+import com.example.group_project_vstu.User;
+import com.example.group_project_vstu.UserDao;
+
+import java.util.List;
+
+public class HomeViewModel extends AndroidViewModel {
 
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<User>> mUsers;
+    private final UserDao userDao;
 
-    public HomeViewModel() {
+    public HomeViewModel(@NonNull Application application) {
+        super(application);
         mText = new MutableLiveData<>();
+        mUsers = new MutableLiveData<>();
         mText.setValue("Привет получилось");
+
+        AppDatabase db = AppDatabase.getDatabase(application);
+        userDao = db.userDao();
+
+        loadUsers();
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<List<User>> getUsers() {
+        return mUsers;
+    }
+
+    private void loadUsers() {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            List<User> users = userDao.getAllUsers();
+            mUsers.postValue(users);
+        });
     }
 }
